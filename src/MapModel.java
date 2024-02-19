9f290f9c3c83017202710763158c15c560c309dc
Jan Navrátil <91787854+Nqjn@users.noreply.github.com>
2024-02-19 16:43:42 +0100

    add functions, setAnswer, addCities and isCorrect

--- src/MapModel.java
+++ src/MapModel.java
@@ -1,12 +1,35 @@
+
+import java.util.LinkedList;
+import java.util.List;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author PC
  */
 public class MapModel {
-    
+
+    private String Answ;
+    private List Cities = new LinkedList<String>();
+
+    public void setAnsw(String Answ) {
+        this.Answ = Answ;
+    }
+
+    public void addCities(List Cities) {
+        this.Cities.add(Cities);
+    }
+
+    /* In MapController will be randomly 
+    picked one city from LinkedList Cities , after the player send his answer
+    this function will compere index of clicked city and index of acctual city
+     */
+    public boolean isCorrect(int AnsIn) {
+        return true;
+
+    }
+
 }
