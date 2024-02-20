7b62faa97976c310124c1e44edce9533a32031ea
Jan Navrátil (2.A) <xnavratil@gml.cz>
2024-02-20 10:26:46 +0100

    Finished isCorrect function

--- src/MapModel.java
+++ src/MapModel.java
@@ -14,21 +14,31 @@ public class MapModel {
 
     private String Answ;
     private List Cities = new LinkedList<String>();
-
+    private int rChosen;
     public void setAnsw(String Answ) {
         this.Answ = Answ;
     }
+    public void setrChosen(int rChosen) {
+        this.rChosen = rChosen;
+    }
+    
 
     public void addCities(List Cities) {
         this.Cities.add(Cities);
+        
     }
 
     /* In MapController will be randomly 
     picked one city from LinkedList Cities , after the player send his answer
     this function will compere index of clicked city and index of acctual city
      */
-    public boolean isCorrect(int AnsIn) {
-        return true;
+    public boolean isCorrect(String Ans) {
+        if(Ans == Cities.get(rChosen)){
+            return true;
+
+
+        }
+        return false;
 
     }
 
