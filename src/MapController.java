622e617af41e7a21ad7ced33789642ab30bed072
Jan Navrátil <91787854+Nqjn@users.noreply.github.com>
2024-02-21 21:03:20 +0100

    first city added

--- src/MapController.java
+++ src/MapController.java
@@ -1,12 +1,55 @@
+
+import java.util.List;
+import java.util.Random;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author PC
  */
 public class MapController {
+
+    Random rand = new Random();
+    MapModel model = new MapModel();
     
+
+    private void chooseCity() {
+        model.setrChosen(rand.nextInt(model.getCities().size()));
+
+    }
+
+    public void setAnsw(int Answ) {
+        model.setAnsw(Answ);
+
+    }
+
+    public List getCity() {
+        return model.getCities();
+
+    }
+
+    public int getrChosen() {
+        return model.getrChosen();
+    }
+    
+        public int getAnsw() {
+        return model.getAnsw();
+    }
+
+    
+        public boolean isCorrect(int AnsIn) {
+            if (model.getCities().get(AnsIn) == model.getCities().get(model.getrChosen())) {
+                return true;
+                
+            }
+            else{
+                return false;
+            }
+
+    }
+
+
 }
--- src/MapModel.java
+++ src/MapModel.java
@@ -1,4 +1,5 @@
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -12,24 +13,69 @@ import java.util.List;
  */
 public class MapModel {
 
-    private String Answ;
-    private List Cities = new LinkedList<String>();
+    private int Answ;
+  
+    private int rChosen;
+    
+          String[] mestaArray = {
+            "Karlovy Vary", "Ostrov n. Ohří", "Chodov", "Sokolov", "Cheb", "Aš", "Mariánské Lázně", "Františkovy Lázně",
+            "Ústí n. Labem", "Žatec", "Louny", "Chomutov", "Most", "Litvínov", "Bílina", "Teplice", "Děčín", "Litoměřice",
+            "Lovosice", "Roudnice n. Labem", "Liberec", "Nový Bor", "Česká Lípa", "Doksy", "Jablonec n. Nisou", "Turnov",
+            "Semily", "Jilemnice", "Hradec Králové", "Jičín", "Vrchlabí", "Trutnov", "Náchod", "Nové Město n. Metují",
+            "Jaroměř", "Dvůr Králové n. Labem", "Rychnov n. Kněžnou", "Pardubice", "Chrudim", "Hlinsko", "Vysoké Mýto",
+            "Ústí n. Orlicí", "Litomyšl", "Česká Třebová", "Lanškroun", "Svitavy", "Moravská Třebová", "Vysočina",
+            "Jihlava", "Třebíč", "Velké Meziříčí", "Žďár n. Sázavou", "Nové Město na Moravě", "Bystřice n. Pernštejnem",
+            "Havlíčkův Brod", "Humpolec", "Pelhřimov", "Chotěboř", "České Budějovice", "Jindřichův Hradec", "Český Krumlov",
+            "Prachatice", "Strakonice", "Písek", "Tábor", "Třeboň", "Týn n. Vltavou", "Bechyně", "Plzeň", "Sušice",
+            "Klatovy", "Domažlice", "Rokycany", "Tachov", "Střední Čechy", "Kladno", "Rakovník", "Příbram", "Kralupy n. Vltavou",
+            "Mělník", "Neratovice", "Brandýs n. Labem", "Nymburk", "Poděbrady", "Kolín", "Kutná Hora", "Čáslav",
+            "Mladá Boleslav", "Benešov", "Vlašim", "Beroun", "Olomouc", "Jeseník", "Šumperk", "Zábřeh", "Mohelnice",
+            "Litovel", "Šternberk", "Prostějov", "Přerov", "Hranice", "Mohelnice", "Morava/Slezsko", "Ostrava", "Karviná",
+            "Opava", "Bohumín", "Havířov", "Český Těšín", "Třinec", "Bruntál", "Krnov", "Frýdek-Místek", "Studénka",
+            "Nový Jičín", "Kopřivnice", "Frenštát p.R.", "Zlín", "Rožnov p.R.", "Valašské Meziříčí", "Vsetín", "Holešov",
+            "Kroměříž", "Otrokovice", "Luhačovice", "Uherské Hradiště", "Uherský brod", "Jižní Morava", "Brno", "Boskovice",
+            "Blansko", "Tišnov", "Kuřim", "Adamov", "Vyškov", "Slavkov u B.", "Bučovice", "Kyjov", "Veselí n. Moravou",
+            "Hodonín", "Břeclav", "Hustopeče", "Mikulov", "Břeclav", "Znojmo", "Moravský Krumlov", "Ivančice", "Rosice"
+        };
 
-    public void setAnsw(String Answ) {
+        // Převod pole na LinkedList
+        LinkedList<String> Cities = new LinkedList<>(Arrays.asList(mestaArray));
+    
+
+    public void setAnsw(int Answ) {
         this.Answ = Answ;
     }
 
-    public void addCities(List Cities) {
+    public void addCities(String Cities) {
         this.Cities.add(Cities);
     }
+
+    public void setrChosen(int rChosen) {
+        this.rChosen = rChosen;
+    }
+
+    public List getCities() {
+        return Cities;
+    }
+
+    public int getrChosen() {
+        return rChosen;
+    }
+    
+    
+
+    
+    
+    
+    
 
     /* In MapController will be randomly 
     picked one city from LinkedList Cities , after the player send his answer
     this function will compere index of clicked city and index of acctual city
      */
-    public boolean isCorrect(int AnsIn) {
-        return true;
 
+    public int getAnsw() {
+        return Answ;
     }
 
 }
